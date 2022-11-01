86be4c328a3c4b469c54d93dad7b69389a80bb6f
CarlosCarela1 <101595703+CarlosCarela1@users.noreply.github.com>
2022-11-01 15:14:34 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,54 @@
+const pokemonContainer = document.querySelector(".pokemon_container")
+
+const llamarData = async (id)=>{
+    pokemones = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    data = await pokemones.json();
+    console.log(data)
+    createPokemon(data)
+}
+
+
+
+llamarPokemones =(num)=>{
+    for(i = 1; i < num; i++){
+        llamarData(i)
+    }
+}
+
+
+
+
+function createPokemon(pokemon){
+    const fragment = document.createDocumentFragment()
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const imgContainer = document.createElement("div")
+    imgContainer.classList.add("img_container")
+
+    const img = document.createElement("img");
+    img.src = pokemon.sprites.front_default;
+
+    const id = document.createElement("p")
+    id.classList.add("id")
+    id.textContent = `ID#` + " " + pokemon.id.toString().padStart(3,0);
+    
+
+    const pokemonName = document.createElement("p");
+    pokemonName.classList.add("parrafo")
+    pokemonName.textContent = pokemon.name;
+
+
+
+    imgContainer.appendChild(img)
+    
+    
+    card.appendChild(imgContainer)
+    card.appendChild(id)
+    card.appendChild(pokemonName)
+    fragment.appendChild(card)
+    pokemonContainer.appendChild(fragment)
+}
+
+
+llamarPokemones(898)
