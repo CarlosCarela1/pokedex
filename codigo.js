03a5c9c09547eeba97631d27a1faa4f80d60efec
CarlosCarela1 <101595703+CarlosCarela1@users.noreply.github.com>
2022-11-02 16:05:14 -0500

    new pokedex

--- codigo.js
+++ codigo.js
@@ -20,6 +20,18 @@ llamarPokemones =(num)=>{
 
 function createPokemon(pokemon){
     const fragment = document.createDocumentFragment()
+    
+    const card_container = document.createElement("div")
+    card_container.classList.add("contenedor_principal")
+
+    const parrafo_back = document.createElement("p")
+    parrafo_back.classList.add("parrafo_back")
+    parrafo_back.innerHTML = `Base Experience: ${pokemon.base_experience}` + `<br/>` +`Type: `+  pokemon.types[0].type.name;
+
+    const back = document.createElement("div")
+    back.classList.add("back")
+    
+
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -36,19 +48,21 @@ function createPokemon(pokemon){
 
     const pokemonName = document.createElement("p");
     pokemonName.classList.add("parrafo")
-    pokemonName.textContent = pokemon.name;
+    pokemonName.innerHTML = `<p style="pokemon-name">${pokemon.name}</p>`;
 
 
 
     imgContainer.appendChild(img)
     
-    
+    back.appendChild(parrafo_back)
     card.appendChild(imgContainer)
     card.appendChild(id)
     card.appendChild(pokemonName)
-    fragment.appendChild(card)
+    card_container.appendChild(card)
+    card_container.appendChild(back)
+    fragment.appendChild(card_container)
     pokemonContainer.appendChild(fragment)
 }
 
 
-llamarPokemones(450)
+llamarPokemones(400)
