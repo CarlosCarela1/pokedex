9f1c61de8294a624b8c930402e9fb8b6604b19ab
CarlosCarela1 <101595703+CarlosCarela1@users.noreply.github.com>
2022-12-02 19:29:35 -0500

    Nueva funcionalidad de buscar actualizada

--- codigo.js
+++ codigo.js
@@ -8,7 +8,6 @@ const llamarData = async (id)=>{
     pokemones = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
     data = await pokemones.json();
     createPokemon(data)
-    console.log(data)
 }
 
 
@@ -22,6 +21,56 @@ const llamarPokemones =(num)=>{
 
 
 function createPokemon(pokemon){
+    let diccionario =["Hierba", "Fuego", "Psiquico", "Electrico", "Tierra", "Roca","Agua", "Lucha", "Insecto", "Veneno", "Hada", "Dragon", "Fantasma", "Hierro", "Hielo", "Oscuro"];
+    let type = pokemon.types[0].type.name;
+    if(type === "fire"){
+        type = diccionario[1]
+    } else if (type === "grass") {
+        type = diccionario[0]
+    }
+    else if (type === "electric") {
+        type = diccionario[3]
+    } 
+    else if (type === "ground") {
+        type = diccionario[4]
+    } 
+    else if (type === "rock") {
+        type = diccionario[5]
+    } 
+    else if (type === "psychic") {
+        type = diccionario[2]
+    } 
+    else if (type === "water") {
+        type = diccionario[6]
+    }
+    else if (type === "fighting") {
+        type = diccionario[7]
+    } 
+    else if (type === "bug") {
+        type = diccionario[8]
+    }
+    else if (type === "poison") {
+        type = diccionario[9]
+    }
+    else if (type === "fairy") {
+        type = diccionario[10]
+    }
+    else if (type === "dragon") {
+        type = diccionario[11]
+    }
+    else if (type === "ghost") {
+        type = diccionario[12]
+    }
+    else if (type === "steel") {
+        type = diccionario[13]
+    }
+    else if (type === "ice") {
+        type = diccionario[14]
+    }
+    else if (type === "dark") {
+        tipoEn = diccionario[15]
+    }
+
     const fragment = document.createDocumentFragment()
     
     const card_container = document.createElement("div")
@@ -29,7 +78,7 @@ function createPokemon(pokemon){
 
     const parrafo_back = document.createElement("p")
     parrafo_back.classList.add("parrafo_back")
-    parrafo_back.innerHTML = `Base Experience: ${pokemon.base_experience}` + `<br/>` +`Type: `+  pokemon.types[0].type.name;
+    parrafo_back.innerHTML = `Experiencia Base: ${pokemon.base_experience}` + `<br/>` +`Tipo: `+  type + `<br/>` + `Peso: `+ pokemon.weight;
 
     const back = document.createElement("div")
     back.classList.add("back")
@@ -72,7 +121,7 @@ function createPokemon(pokemon){
 
 
 
-llamarPokemones(200)
+llamarPokemones(50)
 
 
 
@@ -105,7 +154,57 @@ boton.addEventListener("click", ()=> {
 const Mostrar = (poke)=>{
     "use strict"
     const informacionPokemon = document.getElementById("poke-info");
-    informacionPokemon.innerHTML = `<p class="info-weight"><b>Nombre:</b> ${poke.name}`  + `<br/>` + `<b>Experiencia Base:</b> ${poke.base_experience}` + `<br/>` +`<b>Tipo:</b> `+  `${poke.types[0].type.name}</p>`;
+    
+    let tipoEn = poke.types[0].type.name.toString()
+    let tipo =["Hierba", "Fuego", "Psiquico", "Electrico", "Tierra", "Roca","Agua", "Lucha", "Insecto", "Veneno", "Hada", "Dragon", "Fantasma", "Hierro", "Hielo", "Oscuro"]
+    if(tipoEn === "fire"){
+        tipoEn = tipo[1]
+    } else if (tipoEn === "grass") {
+        tipoEn = tipo[0]
+    }
+    else if (tipoEn === "electric") {
+        tipoEn = tipo[3]
+    } 
+    else if (tipoEn === "ground") {
+        tipoEn = tipo[4]
+    } 
+    else if (tipoEn === "rock") {
+        tipoEn = tipo[5]
+    } 
+    else if (tipoEn === "psychic") {
+        tipoEn = tipo[2]
+    } 
+    else if (tipoEn === "water") {
+        tipoEn = tipo[6]
+    }
+    else if (tipoEn === "fighting") {
+        tipoEn = tipo[7]
+    } 
+    else if (tipoEn === "bug") {
+        tipoEn = tipo[8]
+    }
+    else if (tipoEn === "poison") {
+        tipoEn = tipo[9]
+    }
+    else if (tipoEn === "fairy") {
+        tipoEn = tipo[10]
+    }
+    else if (tipoEn === "dragon") {
+        tipoEn = tipo[11]
+    }
+    else if (tipoEn === "ghost") {
+        tipoEn = tipo[12]
+    }
+    else if (tipoEn === "steel") {
+        tipoEn = tipo[13]
+    }
+    else if (tipoEn === "ice") {
+        tipoEn = tipo[14]
+    }
+    else if (tipoEn === "dark") {
+        tipoEn = tipo[15]
+    }
+    informacionPokemon.innerHTML = `<p class="info-weight"><b>Nombre:</b> ${poke.name}`  + `<br/>` + `<b>Experiencia Base:</b> ${poke.base_experience}` + `<br/>` +`<b>Tipo: </b>${tipoEn} `+ `<br/>`+ `<b>Peso:</b> ${poke.weight.toLocaleString()}kg` +`<br/>` + `<b>Altura: </b> ${poke.height}</p>`;
     let imagen = document.getElementById("imagen-buscador");
     imagen.src = poke.sprites.other.dream_world.front_default;
     imagen.classList.add("imagen-search");
